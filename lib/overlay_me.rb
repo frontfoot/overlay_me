475c2e6b4de51c78ac081fa769f10237ff6288f6
Joseph Boiteau <jo@frontfoot.com.au>
2012-07-24 15:04:58 +1000

    update to handle Rails assets pipeline

--- lib/overlay_me.rb
+++ lib/overlay_me.rb
@@ -8,15 +8,20 @@ module OverlayMe
     def self.call(env)
       Dir.chdir OverlayMe.root_dir if Dir[OverlayMe.root_dir]
       images_urls = Dir[ OverlayMe.overlays_directory + '/**/*.*' ].map{|path| '/'+path}
+      images_urls = images_urls.map{|path| path.sub(/images/, 'assets') } if self.rails_and_assets_pipeline_enabled?
       [200, {"Content-Type" => "text/html"}, images_urls.to_json]
     end
-  end
 
-  module Rails
-    if defined? ::Rails
-      class Engine < ::Rails::Engine
+    def self.rails_and_assets_pipeline_enabled?
+      if defined? ::Rails
+        rails_config = ::Rails.application.config
+        return rails_config.respond_to?(:assets) && rails_config.assets.try(:enabled)
+      else
+        return false
       end
     end
+
   end
+
 end
 
